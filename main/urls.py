ba389d4b70e9452c77bb6ae673d5108348893a8f
brayers <brayers@uwm.edu>
2019-04-20 21:34:41 -0500

    contactInfo base page

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from main import views
+from contactinfo import views as contactInfoviews
 
 urlpatterns = [
   url(r'^admin/', admin.site.urls),
   path("", views.Home.as_view()),
+  path("contactInfo/", contactInfoviews.ContactInfoPage.as_view())
 ]

9aa8ebba6f9102d8ceebd53fa7e33f0d299af2a4
Rex Weber <weber.rex@gmail.com>
2019-04-23 17:53:33 -0500

    Revert "updates"
    This reverts commit 5372043a9629b752f61a36dc9b8cd3bafc48c2fa.

--- acceptance_tests/test_app.py
+++ acceptance_tests/test_app.py
@@ -371,5 +371,5 @@ class TestApp(TestCase):
     def test_unkCommand(self):
         """unrecognized command"""
         cmd = "skjkdhjfhjskhk"
-        result = self.app.command(cmd, request=self.request)
+        result = self.app.command(cmd)
         self.assertEqual(result, "Unrecognized command: " + cmd)

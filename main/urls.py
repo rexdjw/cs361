c060e0acd96b421c565efb609d941b9f68ba1096
Rex Weber <weber.rex@gmail.com>
2019-05-06 22:56:44 -0500

    assign instructors/tas

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic.base import TemplateView
 
-from course.views import ViewCourse
+from course.views import ViewCourse, AssignInstructor, AssignTA
 from main import views
 from contactinfo import views as contactInfoviews
 from course import views as CourseViews
@@ -23,5 +23,7 @@ urlpatterns = [
   re_path(r'users/editaccount', views.EditUsers.as_view(), name='editAccount'),
   re_path(r'users/deleteaccount', views.DeleteUsers.as_view(), name='deleteAccount'),
   url(r'^courses/(?P<course>[a-zA-Z0-9]+)', ViewCourse.as_view(), name="course"),
-  re_path(r'createLab/', LabViews.CreateLabPage.as_view(), name='createLab')
+  re_path(r'createLab/', LabViews.CreateLabPage.as_view(), name='createLab'),
+  url(r'^assignInstructor/(?P<course>[a-zA-Z0-9]+)', AssignInstructor.as_view(), name="course"),
+url(r'^assignTA/(?P<course>[a-zA-Z0-9]+)', AssignTA.as_view(), name="course"),
 ]
